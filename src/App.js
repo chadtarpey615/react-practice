cac2abc000e5ef6ca4b14a1a9e0636ef2598881f
Chad Tarpey <chadtarpey@gmail.com>
2020-12-02 15:57:46 -0600

    Started react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import ImageComponent from "./components/ImageComponent"
+function App() {
+
+  return (
+    <>
+    <div>
+    <h1>Hello!!!</h1>
+    </div>
+
+    <ImageComponent></ImageComponent>
+   
+    </>
+  );
+}
+
+export default App;
